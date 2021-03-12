5e89c5ff285c9a3053251b28b497ef781a355f6d
Steven Duncan <stevenduncan@Macintosh.local>
2021-03-11 21:58:34 -0700

    s

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+// import About from './about.js'
 
 export default function Home() {
   return (
@@ -11,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          COMING SOON...
+         SMYYCO. 
         </h1>
       </main>
 
